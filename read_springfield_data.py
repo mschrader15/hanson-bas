9b1fc6771702539e2cac4746f57b410c658e7bf6
Maxwell Schrader <mcschrader@crimson.ua.edu>
2020-09-22 13:18:03 -0500

    need a pull

--- read_springfield_data.py
+++ read_springfield_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import definitions
 import os
+import urllib3
 
 #Below are the formats to automatical generate the necessary scripts.
 
@@ -22,12 +23,31 @@ files['filter_ip'] = None
 uniqueip = files['ipaddresses'].unique()
 
 for ip in uniqueip:
-    index = 0
+
     for i, val in enumerate(files['ipaddresses'] == ip):
         if val:
-            index = i
             break
-    files.loc[index, 'filter_ip'] = "".join([ip, "_trend.xml"])
+
+    files.loc[i, 'filter_ip'] = "".join([ip, "_trend.xml"])
+
+
+    # This is generating the unique xml functions:
+
+    lines = ['Print " <?xml version=|"1.0|"?>"', '<>']
+    files.loc[files['ipaddresses'] == ip, :].apply(lambda x:  'Print " ')
+
+
+
+
+
+
 
 files.to_excel(excel_file_path)
 
+
+#
+# import requests
+#
+# url = "https://yoursite/your.xml"
+# response = requests.get(url)
+# data = xmltodict.parse(response.content)
