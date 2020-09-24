5e180716c64265e984a8e01c866eaa67919fb60d
mschrader15 <mcschrader@crimson.ua.edu>
2020-09-24 11:59:36 -0500

    Added provision for units to be associated with each measurement

--- functions/logging.py
+++ functions/logging.py
@@ -1,21 +1,18 @@
 import logging
 import os
-from definitions import ROOT, LOGIN_DICT, ERROR_LOGS_DIR
+from definitions import LOGIN_DICT, ERROR_LOGS_DIR
 from logging.handlers import SMTPHandler
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                     filename=os.path.join(ERROR_LOGS_DIR, 'runner.error_log.txt'), filemode="a+", )
 
-
-logger = logging.getLogger(__name__)
-
 mail_handler = SMTPHandler(
     mailhost=(LOGIN_DICT['Logging']['mail_server'], LOGIN_DICT['Logging']['port']),
     fromaddr=LOGIN_DICT['Logging']['email'],
     toaddrs=LOGIN_DICT['Logging']['to_email'],
-    subject='BNSF Sandpoint Application Error',
+    subject='BAS Data Pull Error',
     credentials=(LOGIN_DICT['Logging']['email'], os.environ.get('LOGGING_PASSWORD')),
     secure=())
-
+logger = logging.getLogger(__name__)
 logger.addHandler(mail_handler)
 
