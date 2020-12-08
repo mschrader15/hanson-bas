d0ae4d1778e24ef4f5fb17462c80e22c8f8b88a7
Max Schrader <mcschrader@crimson.ua.edu>
2020-12-08 10:08:40 -0600

    Update logging.py

--- functions/logging.py
+++ functions/logging.py
@@ -6,10 +6,12 @@ from logging.handlers import SMTPHandler
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                     filename=os.path.join(ERROR_LOGS_DIR, 'runner.log'), filemode="a+", )
 
+to_emails = ", ".join([email.strip() for email in LOGIN_DICT['Logging']['to_email'].split(',')])
+
 mail_handler = SMTPHandler(
     mailhost=(LOGIN_DICT['Logging']['mail_server'], LOGIN_DICT['Logging']['port']),
     fromaddr=LOGIN_DICT['Logging']['email'],
-    toaddrs=LOGIN_DICT['Logging']['to_email'],
+    toaddrs=to_emails,
     subject='BAS Data Pull Error',
     credentials=(LOGIN_DICT['Logging']['email'], LOGIN_DICT['Logging']['password']),
     secure=())
